583ca51a3e2a622b948dccc15d6b32f9f7908f43
avinashsaraf <avinashsaraf22@gmail.com>
2023-03-04 16:25:42 +0530

    First commit

---
+++ src/main/java/com/avin/localstack/LocalstackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.avin.localstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalstackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalstackExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avin/localstack/LocalstackExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avin.localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstackExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
