801624fc0c25250b0178fec576392aa9465d1ade
avinashsaraf <avinashsaraf22@gmail.com>
2023-03-18 18:05:26 +0530

    Add bucket user group policy in the sample bucket

--- terraform/aws-localstack/aws-s3-bucket.tf
+++ terraform/aws-localstack/aws-s3-bucket.tf
@@ -14,3 +14,28 @@ resource "aws_s3_bucket_object" "upload-sample-file" {
 output "sample-bucket-arn" {
   value = aws_s3_bucket.create-sample-bucket.arn
 }
+
+resource "aws_iam_group" "s3-sample-user-group" {
+  name = "s3-sample-user-group"
+}
+
+resource "aws_s3_bucket_policy" "sample-group-bucket-policy" {
+  bucket = aws_s3_bucket.create-sample-bucket.id
+  policy = <<EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Aciton": "*",
+              "Effect": "Allow",
+              "Resource": "arn:aws:s3:::${aws_s3_bucket.create-sample-bucket.id}/*",
+              "Principal":{
+                  "AWS":[
+                      "${aws_iam_group.s3-sample-user-group.arn}"
+                  ]
+              }
+          }
+      ]
+  }
+  EOF
+}
